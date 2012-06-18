2489712eef9215f48a4a08db1ac80fe0708c3bb8
curtisfranks <curtisfranks@025980f3-1fbd-cf99-2fc0-d17f21a9f833>
2012-06-18 22:27:33 +0000

    Implemented the RAND command that selects a random move from GetValidMoves and implements it for the current player.

--- trunk/UnitReversi.h
+++ trunk/UnitReversi.h
@@ -30,8 +30,8 @@ enum Square { empty, player1, player2, validMove };
 #define BOLDMAGENTA "\033[1m\033[35m"      
 #define BOLDCYAN    "\033[1m\033[36m"     
 #define BOLDWHITE   "\033[1m\033[37m"      
-//---------------------------------------------------------------------------
-/*/the following is for testing in a windows console
+//-------------------------------------------------------------------------//
+/*the following is for testing in a windows console
 #define RESET   ""
 #define BLACK   ""      
 #define RED     ""      
