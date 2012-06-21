8b54edfc19f9bc26d3af5191c6a90a32a3003bd8
ckvoss@gmail.com <ckvoss@gmail.com@025980f3-1fbd-cf99-2fc0-d17f21a9f833>
2012-06-21 13:32:01 +0000

    -Major Overhaul of GameEngine -Restructured Player Management. Now possible (and easy) for AI v AI -Fully functional HARD AI (beats the snot out of random) (just needs work on optimizing utility function) -TONs of other tweaks, bug fixes, optimizations, and OCD compulsions

--- UnitReversiMain.cpp
+++ UnitReversiMain.cpp
@@ -24,27 +24,48 @@
 
 int boardSize=8,randomMove,isHuman,maxDepth;
 bool displayOn=true,test=false;
-std::string AIlevel = "OFF";
-Square player=player1,AIPlayer=player2,humanPlayer=player1; 
+
+
+//Default case, P1=Black=Human, P2=WHITE=EASYAI. //P1 ALWAYS goes first
+Square CurrentPlayer=player1; //Indicates whose turn it currently is. Game always starts with P1, who is always BLACK.
+const std::string defaultAISetting="EASY";
+std::string AIlevelP1 = "OFF"; //Defaults P2 'OFF' i.e. P2==Human.
+std::string AIlevelP2 = defaultAISetting; ////Defaults P2 to EASY AI.   EASY, MEDIUM, HARD, HARDdebg, ... { P1EASY, P2HARD, P2HARD3,...}(commands. ie to make player2 a hard level AI that looks ahead 3 plys)
+
+bool PlayerIsAI(Square Player){
+    if(Player==player1){return (AIlevelP1!="OFF"); }
+    else if(Player==player2){return (AIlevelP2!="OFF");}
+    else{
+        std::cerr<<"Error: Player passed into --bool isPlayerAI(Square Player)-- must be either player1 or player2.\n";
+        return false;
+    }
+} bool isPlayerAI(Square Player){return PlayerIsAI(Player);}
+
+std::string NullString="NULL";
+std::string& AIlevel(Square Player){ // Getter/Setter  i.e. void MakeP1Human(){ AIlevel(player1)="OFF";}
+    if(!isPlayerAI(Player)){std::cerr<<"Error: Player passed into --std::string& AIlevel(Square Player)-- is not AI.\n"; return NullString;}
+    if(Player==player1){return AIlevelP1;}
+    else if(Player==player2){return AIlevelP2;}
+    else{ std::cerr<<"Error: Player passed into --std::string& AIlevel(Square Player)-- must be either player1 or player2.\n"; return NullString;}
+}
+
 Reversi newBoard(boardSize);
 Reversi game(boardSize);//or you could prompt for the board size with Reversi r(AskUserForBoardSize());
 std::pair<int,int> coordinate,bestMove;
+
 //<< override for Reversi object squares
-std::ostream& operator<<(std::ostream& os, const Square s) 
-{ 
-  switch (s) 
-  { 
-    case empty  : os <<GREEN<<"_"; break; 
-	case validMove: os <<BOLDYELLOW<<"x"<<RESET<<GREEN; break;
-    case player1: os <<BOLDBLACK<<"O"<<RESET<<GREEN; break; //char(2); break; 
-    case player2: os <<BOLDWHITE<<"@"<<RESET<<GREEN; break; //char(1); break; 
-    default: assert(!"Should not get here"); break; 
-  } 
-  return os; 
-} 
+std::ostream& operator<<(std::ostream& os, const Square s) { 
+    switch(s){
+        case empty  : os <<GREEN<<"_"; break; 
+        case validMove: os <<BOLDYELLOW<<"x"<<RESET<<GREEN; break;
+        case player1: os <<BOLDBLACK<<"O"<<RESET<<GREEN; break; //char(2); break; 
+        case player2: os <<BOLDWHITE<<"@"<<RESET<<GREEN; break; //char(1); break; 
+        default: assert(!"Should not get here"); break; 
+    }
+    return os; 
+}
 //<< override for Reversi object board outlay
-std::ostream& operator<<(std::ostream& os, const Reversi& r) 
-{ 
+std::ostream& operator<<(std::ostream& os, const Reversi& r) { 
   { //Show the indices horizontally 
     const int size = r.GetSize(); 
     os << "  "; 
@@ -95,8 +116,7 @@ const bool IsInt(const std::string& s, int& rInt)
   return true; 
 } 
 //Handles all input and drops the newline char
-const std::string GetInput() 
-{ 
+const std::string GetInput(){
 	char c;
 	std::string s; 
 	std::getline(std::cin,s,'\n'); 
@@ -104,12 +124,10 @@ const std::string GetInput()
 		c=s[i];
 		s[i]=toupper(c);
 	}
-
 	return s; 
-} 
+}
 //Breaks up the coordinate input for an x and y value
-const std::vector<std::string> SeperateString(std::string input, const char seperator) 
-{ 
+const std::vector<std::string> SeperateString(std::string input, const char seperator){
   assert(input.empty()==false); 
   assert(input[0]!=seperator); 
   assert(input[input.size()-1]!=seperator); 
@@ -129,9 +147,9 @@ const std::vector<std::string> SeperateString(std::string input, const char sepe
   } 
   result.push_back(input); 
   return result; 
-} 
+}
 //Up front determination whether the users coordinate is a valid input type 
-const bool IsCoordinate(const std::string& input, std::pair<int,int>& coordinate) {
+const bool IsCoordinate(const std::string& input, std::pair<int,int>& coordinate){
     if(input.size()!=2) return false;
 	//TODO: need error (bounds) checking on x and y
     int x, y;
@@ -142,11 +160,9 @@ const bool IsCoordinate(const std::string& input, std::pair<int,int>& coordinate
     return true;
 } 
 //Optional menu option allowing for board sizes between 4X4 and 16X16
-const int AskUserForBoardSize() 
-{ 
+const int AskUserForBoardSize(){
   //Get the board's size 
-  while (1) 
-  { 
+  while (1){
     std::cout << "Please enter the size of the reversi board" << std::endl; 
     const std::string input = GetInput(); 
     int size = -1; 
@@ -167,70 +183,128 @@ const int AskUserForBoardSize()
     } 
     return size; 
   } 
-} 
+}
 
 int handlePregameInput(){
 	while(1){
 		const std::string input = GetInput();
-		if(input=="EXIT")
+		if(input=="EXIT"){
 			return 0;
-		if(input=="TEST_HARD"){
-			AIlevel="HARD";
-			test=true;
-			std::cout<<"HARD V HARD\n";
 		}
-		if(input=="TEST_MEDIUM"){
-			AIlevel="MEDIUM";
-			test=true;
-			std::cout<<"HARD V MEDIUM\n";
-		}
-		if(input=="TEST_EASY"){
-			AIlevel="EASY";
-			test=true;
-			std::cout<<"HARD V EASY\n";
-		}
-		if(input=="BLACK"){
-			player = player1;
-			AIPlayer=player2;
-			std::cout<<"BLACK\n";
-		}
-		if(input=="WHITE"){
-			player = player2;
-			AIPlayer=player1;
-			std::cout<<"WHITE\n";
-		}
-		if(input=="EASY"){
-			AIlevel="EASY";
-			std::cout<<"OK\n";
-		}
-		if(input=="MEDIUM"){
-			AIlevel="MEDIUM";
-			std::cout<<"OK\n";
-		}
-		if(input=="HARD"){
-			AIlevel="HARD";
-			std::cout<<"OK\n";
-		}
-		if(input=="DISPLAY_ON" || input =="1"){
+		else if(input=="DISPLAY_ON" ){
 			displayOn = true;
 			std::cout<<"OK\n";
 			break;
 		}
-		if(input=="DISPLAY_OFF"){
+		else if(input=="DISPLAY_OFF"){
 			displayOn = false;
 			std::cout<<"OK\n";
 			break;
 		}
-		if(input=="4X4"){
+		else if(input=="BLACK"){
+		    AIlevelP2=AIlevelP1;
+		    AIlevelP1="OFF";
+		}
+		else if(input=="TEST_HARD"){
+			AIlevelP1="HARD";
+			AIleveP2="HARD"
+			std::cout<<"HARD V HARD\n";
+		}
+		else if(input=="TEST_MEDIUM"){
+			AIlevelP1="HARD";
+			AIlevelP2="MEDIUM";
+			std::cout<<"HARD V MEDIUM\n";
+		}
+		else if(input=="TEST_EASY"){
+			AIlevelP1="HARD";
+			AIlevelP2="EASY";
+			std::cout<<"HARD V EASY\n";
+		}
+		if(input=="BLACK"){
+			AIlevelP2=AIlevelP1;
+		    AIlevelP1="OFF";
+			std::cout<<"BLACK\n";
+		}
+		else if(input=="WHITE"){
+		    AIlevelP1=AIlevelP2;
+		    AIlevelP2="OFF";
+			std::cout<<"WHITE\n";
+		}
+		else if(input=="NO_AI"){
+		    AIlevelP1="OFF";
+		    AIlevelP2="OFF";
+		    std::cout<<"OK\n";
+		}
+		else if(input=="EASY"){
+		    Square aiPlayer = ((PlayerIsAI(player2))?(player2):(player1));
+		    AIlevel(aiPlayer) ="EASY";
+			std::cout<<"OK\n";
+		}
+		else if(input=="MEDIUM"){
+		    Square aiPlayer = ((PlayerIsAI(player2))?(player2):(player1));
+		    AIlevel(aiPlayer) ="MEDIUM";
+			std::cout<<"OK\n";
+		}
+		else if(input=="HARD"){
+		    Square aiPlayer = ((PlayerIsAI(player2))?(player2):(player1));
+		    AIlevel(aiPlayer) ="HARD";
+			std::cout<<"OK\n";
+		}
+		else if(input[0]=='P'){
+		    if(input[1]=='1'){
+		        AIlevelP1=input.substr(2);
+                std::cout<<"OK\n";
+		    }
+		    else if(input[1]=='2'){
+		        AIlevelP2=input.substr(2);
+                std::cout<<"OK\n";
+		    }
+		    else{std::cout<<"ILLEGAL\n";}
+		}
+		else if(input=="4X4"){
 			std::cout<<"OK\n";
 			boardSize=4;
 		}
-		if(input=="8X8"){
+
+		else if(input=="8X8"){
 			std::cout<<"OK\n";
 			boardSize=8;
 		}
-		if(input=="?"){
-			std::cout<<"WHITE, BLACK, EASY, MEDIUM, HARD, DISPLAY_ON, EXIT\n";
+		else if(input=="6"){
+		    AIlevelP1="HARD";
+		    AIlevelP2="OFF";
+		    std::cout<<"OK\n";
+			std::cout<<"P1:HARD-AI, P2:Human\n";
+		    displayOn=true;
+			break;
+		}
+		else if(input=="7"){
+		    AIlevelP2="HARD";
+		    AIlevelP1="OFF";
+		    std::cout<<"OK\n";
+			std::cout<<"P1:Human, P2:HARD-AI\n";
+		    displayOn=true;
+			break;
+		}
+		else if(input=="8"){
+		    AIlevelP1="HARD";
+		    AIlevelP2="EASY";
+		    std::cout<<"OK\n";
+			std::cout<<"P1:"<<AIlevelP1<<"-AI, P2:"<<AIlevelP2<<"-AI\n";
+		    displayOn=true;
+			break;
+		}
+		else if(input=="88"){
+		    AIlevelP1="HARD_DEBG";
+		    AIlevelP2="EASY";
+		    std::cout<<"OK\n";
+			std::cout<<"P1:"<<AIlevelP1<<"-AI, P2:"<<AIlevelP2<<"-AI\n";
+		    displayOn=true;
+			break;
+		}
+		else{
+		    if(input!="?"){std::cout<<"ILLEGAL\n";}
+			std::cout<<"WHITE, BLACK, EASY, MEDIUM, HARD, DISPLAY_ON, EXIT\n"; //TODO: Add more..
 		}
 	}
 	return 1;
@@ -239,7 +313,7 @@ int handlePregameInput(){
 void showPossibleMoves(){
 	Reversi tempValid(boardSize);
 	tempValid.SetBoard(game.GetBoard());
-	std::vector< std::pair<int,int> > vals = tempValid.GetValidMoves(player);
+	std::vector< std::pair<int,int> > vals = tempValid.GetValidMoves(CurrentPlayer);
 	for(int i=0; i<vals.size(); i++)
 		tempValid.SetSquare(vals[i].first,vals[i].second,validMove);
 	std::cout<<tempValid<<"\n";
@@ -249,7 +323,7 @@ void moveRandomly(){
 	srand ( time(NULL) );
 	Reversi tempValid(boardSize);
 	tempValid.SetBoard(game.GetBoard());
-	std::vector< std::pair<int,int> > vals = tempValid.GetValidMoves(player);
+	std::vector< std::pair<int,int> > vals = tempValid.GetValidMoves(CurrentPlayer);
 	int randomMove = rand() % vals.size();
 	coordinate.first=vals[randomMove].first;coordinate.second=vals[randomMove].second;
 }
@@ -384,122 +458,139 @@ std::pair<int,int> findBestMove(Square forecastPlayer){
 }
 
 int handleGameInput(){
-	while(1){
-		if(displayOn==true)
-			std::cout<< game;
-		if	(game.Count(empty) == 0||//No more empty squares or niether player has a move, end game 
-			((game.GetValidMoves(player1).empty()==true)&&(game.GetValidMoves(player2).empty()==true))){ 
-		  const int n1 = game.Count(player1); 
-		  const int n2 = game.Count(player2); 
-		  std::cout << "The game has ended." << std::endl 
-			<< "Player 1 conquered " << n1 << " squares." << std::endl 
-			<< "Player 2 conquered " << n2 << " squares." << std::endl;
-			if(n1!=n2){
-			std::cout<< "The winner is player " << (n1 > n2 ? "1" : "2") << std::endl 
-			<< "Congratulations!" << std::endl;
-			}
-			else
-				std::cout<<"It's a draw!\n";
-		    std::cout << "Press ENTER to continue... ";
-			std::cin.ignore( std::numeric_limits <std::streamsize> ::max(), '\n' );
-			test=false;
-			AIlevel="OFF";
-			game.SetBoard(newBoard.GetBoard());
-		  return 1;
-		} 
-		if (game.GetValidMoves(player).empty()==true){ //Check if current player can move 
-		  std::cout << "Too bad! Player " << player << " is unabled to do a valid move!"; 
-		  player = (player == player1 ? player2 : player1); 
-		  std::cout << "\nThe next turn again goes to player " << player << "!" << std::endl; 
-		  continue; 
-		} 
-		std::cout<<"Current Player:"<<player<<"\n";//Who's turn is it?
-		if(AIlevel!="OFF"&&player==AIPlayer){//Which AI to use
-			if(AIlevel=="EASY")
-				moveRandomly();
-			if(AIlevel=="MEDIUM"){
-				maxDepth=2;
-				std::pair<int,int> bestMove = findBestMove(player);
-				coordinate.first=bestMove.first;coordinate.second=bestMove.second;
-			}
-			if(AIlevel=="HARD"){
-				HardAI hAI(game, player);
-				coordinate = hAI.findMax();
-				cout<<"MaxFound:"<<coordinate.first<<","<<coordinate.second<<endl;
-			}
-		}
-		if(test==true&&player==player1){
-			maxDepth=2;
-			std::pair<int,int> bestMove = findBestMove(player);
-			coordinate.first=bestMove.first;coordinate.second=bestMove.second;
-		}
-		else if(AIlevel!="OFF"&&player!=AIPlayer&&test==false) {
-			const std::string input = GetInput();
-			const bool isValidCoordinate = IsCoordinate(input, coordinate); 
-			if (isValidCoordinate == false){
-				if(input=="EXIT")
-					return 0;
-				else if(input=="?"){
-					std::cout<<"Enter coordinates as # alpha values, DISPLAY_OFF, SHOW_NEXT_POS, UNDO, REDO, EXIT\n";
-					continue;
-				}
-				else if(input=="DISPLAY_OFF"){
-					displayOn = false;
-					std::cout<<"OK\n";
-					continue;
-				}
-				if(input=="UNDO"){
-					if(!game.DoUndo()){
-						assert(!"CANNOT UNDO, NOT ENOUGH STATES IN STACK");
-						std::cout<<"ILLEGAL\n";
-						continue;
-					}continue;
-				}else if(input=="REDO"){
-					if(!game.DoRedo()){
-						assert(!"CANNOT REDO, NOT ENOUGH STATES IN STACK");
-						std::cout<<"ILLEGAL\n";
-						continue;
-					}continue;
-				}else if(input=="SHOW_NEXT_POS"){
-					showPossibleMoves();
-					continue;
-				}else if(input=="RAND"){
-					moveRandomly();
-				}else{
-					std::cout << "ILLEGAL\n";
-					continue;
-				} 
-			}
-		} 
-		if (game.IsValidMove(coordinate.first, coordinate.second, player) == false){ 
-		  std::cout << "ILLEGAL\n";
-		  continue; 
-		}
-		game.DoMove(coordinate.first, coordinate.second, player); //Actually do the move 
-		player = (player == player1 ? player2 : player1);//switch to next player
-	}
-	return 1;
+    while(1){
+        if(displayOn){
+            std::cout<<"\n-------------------\n";
+            std::cout<<"Current Player:"<<CurrentPlayer<<"\n";
+            std::cout<<((PlayerIsAI(CurrentPlayer))?("Waiting on AI"):("Human's Move"))<<"\n";
+            std::cout<< game;
+        }
+
+        //Check if the game has ended 
+        if (game.Count(empty) == 0){
+            //No empty squares 
+            const int n1 = game.Count(player1); 
+            const int n2 = game.Count(player2); //TODO: ADD IN TIE CASE
+            std::string p1Name = (PlayerIsAI(player1))?(AIlevelP1+"-AI"):("Human");
+            std::string p2Name = (PlayerIsAI(player2))?(AIlevelP2+"-AI"):("Human");
+            std::cout<< "The game has ended\n"
+                    <<"Player1 ("<<p1Name<<")["<<player1<<"] conquered "<<n1<<" squares.\n"
+                    <<"Player2 ("<<p2Name<<")["<<player2<<"] conquered "<<n2<<" squares.\n"
+                    <<"The winner is Player"<<((n1>n2)?("1("+p1Name+")"):("2("+p2Name+")"))
+                    <<"\nCongratulations!\n\n";
+             return 0;
+        }
+
+        //Check if Current Player can actually make a move 
+        if (game.GetValidMoves(CurrentPlayer).empty()==true){
+            //If Current Player cannot move,
+            std::cout<<"Too bad! Player"<<int(CurrentPlayer)<<"("<<CurrentPlayer<<") is unabled to do a valid move!\n"; 
+            CurrentPlayer=GetOtherPlayer(CurrentPlayer);
+            std::cout<<"The next turn again goes to Player"<<int(CurrentPlayer)<<"("<<CurrentPlayer<<") !\n";
+            continue;
+        }
+        
+        {//Input-Gathering Block
+            if(PlayerIsAI(CurrentPlayer)){
+                //CurrentPlayer is an AI
+                if(AIlevel(CurrentPlayer)=="EASY"){
+                    moveRandomly();
+                }
+                else if(AIlevel(CurrentPlayer)=="MEDIUM"){
+                    moveDepth=0;
+                    std::pair<int,int> bestMove = checkForMax(game,CurrentPlayer);
+                    coordinate.first=bestMove.first;coordinate.second=bestMove.second;
+                }
+                else if(AIlevel(CurrentPlayer).substr(0,4)=="HARD"){
+                    HardAI hAI(game, CurrentPlayer,1,AIlevel(CurrentPlayer).substr(AIlevel(CurrentPlayer).size()-4)=="DEBG");
+                    coordinate = hAI.findMax();
+                }
+                std::cout<<AIlevel(CurrentPlayer)<<"-AI Plays @:"<<char('A'+coordinate.first)<<coordinate.second+1<<endl;
+            }
+            else if( !(PlayerIsAI(CurrentPlayer)) ) {
+                //CurrentPlayer is a Human
+                std::cout<<"::";
+                const std::string input = GetInput();
+                const bool isValidCoordinate = IsCoordinate(input, coordinate); 
+                if (isValidCoordinate == false){
+                    //Input was not a Coordinate
+                    if(input=="EXIT") return 0;
+                    else if(input=="?"){
+                        std::cout<<"Enter coordinates as # alpha values, DISPLAY_OFF, SHOW_NEXT_POS, UNDO, REDO, EXIT\n";
+                        continue;
+                    }
+                    else if(input=="DISPLAY_OFF"){
+                        displayOn = false;
+                        std::cout<<"OK\n";
+                        continue;
+                    }
+                    else if(input=="UNDO"){
+                        if(!game.DoUndo()){
+                            std::cerr<<"CANNOT UNDO, NOT ENOUGH STATES IN STACK\n";
+                            std::cout<<"ILLEGAL\n";
+                            continue;
+                        }
+                        continue;
+                    }
+                    else if(input=="REDO"){
+                        if(!game.DoRedo()){
+                            std::cerr<<"CANNOT REDO, NOT ENOUGH STATES IN STACK\n";
+                            std::cout<<"ILLEGAL\n";
+                            continue;
+                        }
+                        continue;
+                    }
+                    else if(input=="SHOW_NEXT_POS"){
+                        showPossibleMoves();
+                        continue;
+                    }
+                    else if(input=="RAND" || input=="R"){
+                        moveRandomly();
+                    }
+                    else{
+                        std::cout << "ILLEGAL\n";
+                        continue;
+                    }
+                }
+                if(isValidCoordinate){
+                   //Valid coordinate was entered
+                    if(displayOn){
+                        std::cout<<"Human Plays @:"<<char('A'+coordinate.first)<<coordinate.second+1<<endl;
+                    }
+                }
+            }//End Human Input
+        }//End of Input-Gathering Block
+        
+        //Check that move is valid for Current Player.
+        if (game.IsValidMove(coordinate.first, coordinate.second, CurrentPlayer) == false){
+            std::cout << "ILLEGAL\n";
+            continue; 
+        }
+
+        //Actually do the move 
+        game.DoMove(coordinate.first, coordinate.second, CurrentPlayer);
+
+        //Switch Player's turns
+        CurrentPlayer=GetOtherPlayer(CurrentPlayer); 
+        if(displayOn){std::cout<<"-------------------\n\n";}
+    }
+    return 0;
 }
 //Handles user input and display of data 
-int api(std::string commandLine)
-{
-	while(1){
-		for(int i=0;i<100;i++)std::cout<<"\n";
-		std::cout<< "WELCOME\n";
-		if(!handlePregameInput())
-			return 0;
-		std::cout<<"Player1"<<"BLACK"<<player1<<"\n"<<RESET;
-		std::cout<<"Player2"<<"WHITE"<<player2<<"\n"<<RESET;
-		if(!handleGameInput())
-			return 0;
-	}
+int api(std::string commandLine){
+	for(int i=0;i<100;i++)std::cout<<"\n";
+	std::cout<< "WELCOME\n";
+	if(!handlePregameInput()){return 0;}
+	
+	//Player"<<int(player)<<"("<<player<<") is 
+	std::cout<<"Player1: "<<((PlayerIsAI(player1))?("AI    :"):("Human :"))<<player1<<": BLACK\n"<<RESET;
+	std::cout<<"Player2: "<<((PlayerIsAI(player2))?("AI    :"):("Human :"))<<player2<<": WHITE\n"<<RESET;
+	if(!handleGameInput()){return 0;}
 }
 
-int main() 
-{
+int main() {
 	api("START");
 	return 0;
 }
-
 
 
