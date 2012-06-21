f41ea10e7eb36952872479cda993a896713ff3d9
curtisfranks <curtisfranks@025980f3-1fbd-cf99-2fc0-d17f21a9f833>
2012-06-21 11:34:51 +0000

    Improved the medium AI, but now it's slower than a sloth.

--- trunk/UnitReversiMain.cpp
+++ trunk/UnitReversiMain.cpp
@@ -16,15 +16,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <limits>
 #include <cctype>//for upper to lower
 #include "UnitReversi.h"//#includes <vector>
-#include "AI.h"
+//#include "AI.h"
 //global variables - created to prepare for port to Java
 
-int boardSize=8,randomMove,isHuman,moveDepth;
-bool displayOn=true;
+int boardSize=8,randomMove,isHuman,maxDepth;
+bool displayOn=true,test=false;
 std::string AIlevel = "OFF";
 Square player=player1,AIPlayer=player2,humanPlayer=player1; 
+Reversi newBoard(boardSize);
 Reversi game(boardSize);//or you could prompt for the board size with Reversi r(AskUserForBoardSize());
 std::pair<int,int> coordinate,bestMove;
 //<< override for Reversi object squares
@@ -172,6 +174,21 @@ int handlePregameInput(){
 		const std::string input = GetInput();
 		if(input=="EXIT")
 			return 0;
+		if(input=="TEST_HARD"){
+			AIlevel="HARD";
+			test=true;
+			std::cout<<"HARD V HARD\n";
+		}
+		if(input=="TEST_MEDIUM"){
+			AIlevel="MEDIUM";
+			test=true;
+			std::cout<<"HARD V MEDIUM\n";
+		}
+		if(input=="TEST_EASY"){
+			AIlevel="EASY";
+			test=true;
+			std::cout<<"HARD V EASY\n";
+		}
 		if(input=="BLACK"){
 			player = player1;
 			AIPlayer=player2;
@@ -237,150 +254,192 @@ void moveRandomly(){
 	coordinate.first=vals[randomMove].first;coordinate.second=vals[randomMove].second;
 }
 
-/*std::pair<int,int> forcastThenMove(Reversi previousBoard,Square forcastPlayer){
-	std::pair<int,int> countFromForcast;
-	std::pair<int,int> returnFromForcast;
-	std::pair<int,int> forcastCoordinate;
+int checkForWeight(Reversi parentBoard, Square forecastPlayer,int depth){
+	
+	int endDepthScore=-99999,minCount=64,minPossibleMove=0,maxPossibleMove=0;
 	Square nextPlayer;
-	int highestDifferential=0;
-	Reversi tempValid(boardSize);
-	tempValid.SetBoard(previousBoard.GetBoard());
-	std::vector< std::pair<int,int> > vals = tempValid.GetValidMoves(forcastPlayer);
-	if (tempValid.Count(empty) == 0){
-			countFromForcast.first = tempValid.Count(player1); 
-			countFromForcast.second = tempValid.Count(player2); 
-			return countFromForcast;
-	}
-	if(vals.size()==0){
-		nextPlayer = (forcastPlayer == player1 ? player2 : player1);
-		returnFromForcast = forcastThenMove(tempValid,nextPlayer);
-	}
-	for(int i=0;i<vals.size();i++){
-		tempValid.DoMove(vals[i].first, vals[i].second, forcastPlayer); 
-		std::cout<< tempValid;
-		nextPlayer = (forcastPlayer == player1 ? player2 : player1);
-		moveDepth++;
-		if(moveDepth<10)
-			returnFromForcast = forcastThenMove(tempValid,nextPlayer);
-		if(player==player1){
-			if(returnFromForcast.first>returnFromForcast.second){
-				if(abs(returnFromForcast.first-returnFromForcast.second)>highestDifferential){
-					highestDifferential = abs(returnFromForcast.first-returnFromForcast.second);
-					bestMove=vals[i];
-				}
-				return returnFromForcast;
-			}
+	if(forecastPlayer!=player&&parentBoard.GetValidMoves(forecastPlayer).empty()==true)
+		return 10000;
+	else if(forecastPlayer==player&&parentBoard.GetValidMoves(forecastPlayer).empty()==true)
+		return -10000;
+	std::vector< std::pair<int,int> >vals=parentBoard.GetValidMoves(forecastPlayer);
+	for(int possibleMove=0;possibleMove<vals.size();possibleMove++){
+		int maxCount=0;
+		Square opponent;
+		Reversi childBoard(boardSize);				//making one childBoard for each 
+		childBoard.SetBoard(parentBoard.GetBoard());//possible Move from the parentBoard
+		childBoard.DoMove(vals[possibleMove].first,vals[possibleMove].second,forecastPlayer);//add this move to its board
+		if(depth<maxDepth){
+			nextPlayer = (forecastPlayer == player1 ? player2 : player1);
+			maxCount=maxCount+checkForWeight(childBoard,nextPlayer,depth+1);
 		}
-		if(player==player2){
-			if(returnFromForcast.first<returnFromForcast.second){
-				if(abs(returnFromForcast.first-returnFromForcast.second)>highestDifferential){
-					highestDifferential = abs(returnFromForcast.first-returnFromForcast.second);
-					bestMove=vals[i];
-				}
-				return returnFromForcast;
-			}
+		else{
+			return childBoard.Count(forecastPlayer);
 		}
-	}
-	return returnFromForcast;
 
-}*/
-int checkForMin(Reversi tempGame, Square forcastPlayer){
-	Reversi tempValid(boardSize);
-	int count=64,minPossibleMove=0;
-	Square nextPlayer;
-	tempValid.SetBoard(tempGame.GetBoard());
-	std::vector< std::pair<int,int> > vals = tempValid.GetValidMoves(forcastPlayer);
-	for(int possibleOpponentMove=0;possibleOpponentMove<vals.size();possibleOpponentMove++){
-		Reversi tempValida(boardSize);
-		tempValida.SetBoard(tempGame.GetBoard());
-
-		tempValida.DoMove(vals[possibleOpponentMove].first,vals[possibleOpponentMove].second, forcastPlayer);
-//		std::cout<<tempValida<<possibleOpponentMove<<"\n";
-		if(tempValida.Count(forcastPlayer)<count){
-			count=tempValida.Count(forcastPlayer);
-			minPossibleMove=possibleOpponentMove;
+		if	(childBoard.Count(empty) == 0||//No more empty squares or niether player has a move, end game 
+			((childBoard.GetValidMoves(player1).empty()==true)&&
+			(childBoard.GetValidMoves(player2).empty()==true))){ 
+				if(player==player1)
+					opponent=player2;
+				else
+					opponent=player1;
+				if(childBoard.Count(player)>childBoard.Count(opponent))
+					maxCount=maxCount+10000;
+				else
+					maxCount=maxCount-10000;
+		}else if(forecastPlayer!=player&&childBoard.GetValidMoves(forecastPlayer).empty()==true){
+			maxCount=maxCount+10000;
+		}else if(forecastPlayer==player){
+			if((vals[possibleMove].first==1&&
+				(vals[possibleMove].second!=0&&vals[possibleMove].second!=7))||
+				(vals[possibleMove].first==6&&
+				(vals[possibleMove].second!=0&&vals[possibleMove].second!=7)))
+					maxCount=maxCount-1000;
+			if((vals[possibleMove].second==1&&
+				(vals[possibleMove].first!=0&&vals[possibleMove].first!=7))||
+				(vals[possibleMove].second==6&&
+				(vals[possibleMove].first!=0&&vals[possibleMove].first!=7)))
+					maxCount=maxCount-1000;
+			if(vals[possibleMove].first==0||vals[possibleMove].first==7)
+				maxCount=maxCount+1000;
+			if(vals[possibleMove].second==0||vals[possibleMove].second==7)
+				maxCount=maxCount+1000;
+			if((vals[possibleMove].first==0&&vals[possibleMove].second==0)||
+				(vals[possibleMove].first==7&&vals[possibleMove].second==7)||
+				(vals[possibleMove].first==0&&vals[possibleMove].second==7)||
+				(vals[possibleMove].first==7&&vals[possibleMove].second==0))
+					maxCount=maxCount+5000;
+		}else{
+			if((vals[possibleMove].first==1&&
+				(vals[possibleMove].second!=0&&vals[possibleMove].second!=7))||
+				(vals[possibleMove].first==6&&
+				(vals[possibleMove].second!=0&&vals[possibleMove].second!=7)))
+					maxCount=maxCount+100;
+			if((vals[possibleMove].second==1&&
+				(vals[possibleMove].first!=0&&vals[possibleMove].first!=7))||
+				(vals[possibleMove].second==6&&
+				(vals[possibleMove].first!=0&&vals[possibleMove].first!=7)))
+					maxCount=maxCount+100;
+			if(vals[possibleMove].first==0||vals[possibleMove].first==7)
+				maxCount=maxCount-1000;
+			if(vals[possibleMove].second==0||vals[possibleMove].second==7)
+				maxCount=maxCount-1000;
+			if((vals[possibleMove].first==0&&vals[possibleMove].second==0)||
+				(vals[possibleMove].first==7&&vals[possibleMove].second==7)||
+				(vals[possibleMove].first==0&&vals[possibleMove].second==7)||
+				(vals[possibleMove].first==7&&vals[possibleMove].second==0))
+					maxCount=maxCount-5000;
+			if(childBoard.GetValidMoves(forecastPlayer).empty()==true)
+				maxCount=maxCount+1000;
 		}
-	}
-	return count;
-}
-std::pair<int,int> checkForMax(Reversi tempGame, Square forcastPlayer){
-	Reversi tempValid(boardSize);
-	int countMax=0,countMin=0,maxPossibleMove=0;
-	Square nextPlayer;
-	tempValid.SetBoard(tempGame.GetBoard());
-	std::vector< std::pair<int,int> > vals = tempValid.GetValidMoves(forcastPlayer);
-	for(int possibleMove=0;possibleMove<vals.size();possibleMove++){														
-		Reversi tempValidb(boardSize);
-		tempValidb.SetBoard(tempGame.GetBoard());
 
 
-		tempValidb.DoMove(vals[possibleMove].first, vals[possibleMove].second, forcastPlayer);
-//		std::cout<<tempValidb<<possibleMove<<"\n";
-		nextPlayer = (forcastPlayer == player1 ? player2 : player1);
-		countMin=checkForMin(tempValidb,nextPlayer);
-		if(countMin>countMax){
-			countMax=countMin;
-			maxPossibleMove=possibleMove;
-		}
+		if(maxCount>endDepthScore)
+			endDepthScore=maxCount;
 	}
-	return vals[maxPossibleMove];
+	return endDepthScore;
 }
 
-std::pair<int,int> minMax(Reversi tempGame, Square tempPlayer){
-	//check for valid moves first, if none just send turn to next player
+std::pair<int,int> findBestMove(Square forecastPlayer){
+	int maxCountForAll=-99999,maxCountAtDepth=-99999,maxPossibleMove=0;
+	Square nextPlayer;
+	std::vector< std::pair<int,int> >vals=game.GetValidMoves(forecastPlayer);//possible moves for current player
+	for(int possibleMove=0;possibleMove<vals.size();possibleMove++){	
+		int primaryWeight=0;
+		Reversi boardForAPrimaryMove(boardSize);
+		boardForAPrimaryMove.SetBoard(game.GetBoard());//new board (branch to tree) for each possibleMove created each itteration
+		boardForAPrimaryMove.DoMove(vals[possibleMove].first, vals[possibleMove].second, forecastPlayer);//add this possible move to its board
+		if(	(vals[possibleMove].first==1&&
+			(vals[possibleMove].second!=0&&vals[possibleMove].second!=7))||
+			(vals[possibleMove].first==6&&
+			(vals[possibleMove].second!=0&&vals[possibleMove].second!=7)))
+				primaryWeight=primaryWeight-5000;
+		if(	(vals[possibleMove].second==1&&
+			(vals[possibleMove].first!=0&&vals[possibleMove].first!=7))||
+			(vals[possibleMove].second==6&&
+			(vals[possibleMove].first!=0&&vals[possibleMove].first!=7)))
+				primaryWeight=primaryWeight-5000;
+		if(vals[possibleMove].first==0||vals[possibleMove].first==7)
+				primaryWeight=primaryWeight+2000;
+		if(vals[possibleMove].second==0||vals[possibleMove].second==7)
+				primaryWeight=primaryWeight+2000;
+		if(	(vals[possibleMove].first==0&&vals[possibleMove].second==0)||
+			(vals[possibleMove].first==7&&vals[possibleMove].second==7)||
+			(vals[possibleMove].first==0&&vals[possibleMove].second==7)||
+			(vals[possibleMove].first==7&&vals[possibleMove].second==0))
+				primaryWeight=primaryWeight+5000;
+		
+		
+		nextPlayer = (forecastPlayer == player1 ? player2 : player1);//figure out who is the next player
+		maxCountAtDepth=primaryWeight+checkForWeight(boardForAPrimaryMove,nextPlayer,0);//the maximum outcome from the set Depth (checkForWeight())
+		if(maxCountAtDepth>maxCountForAll){	//if this possible Move's maximum outcome is bigger
+			maxCountForAll=maxCountAtDepth;	//keep it as the new maximum overall
+			maxPossibleMove=possibleMove;	//and remember which move that maximum belongs to
+		}
+	}
+	std::cout<<"Value of this move:"<<maxCountForAll<<std::endl;
+	return vals[maxPossibleMove];//when done checking down each branch of possible Moves send the best move back
 
-	std::pair<int,int> temp;
-
-	return temp;
 }
 
 int handleGameInput(){
 	while(1){
-		std::cout<<"Current Player:"<<player<<"\n";
 		if(displayOn==true)
 			std::cout<< game;
-		if(AIlevel!="OFF"&&player==AIPlayer){
-			if(AIlevel=="EASY")
-				moveRandomly();
-			if(AIlevel=="MEDIUM"){
-				moveDepth=0;
-				std::pair<int,int> bestMove = checkForMax(game,player);
-				coordinate.first=bestMove.first;coordinate.second=bestMove.second;
-			}
-			if(AIlevel=="HARD"){
-				HardAI hAI(game, player);
-				coordinate = hAI.findMax();
-				cout<<"MaxFound:"<<coordinate.first<<","<<coordinate.second<<endl;
-			}
-		}
-		//Check if the game has ended 
-		if (game.Count(empty) == 0) 
-		{ 
-		  //No empty squares 
+		if	(game.Count(empty) == 0||//No more empty squares or niether player has a move, end game 
+			((game.GetValidMoves(player1).empty()==true)&&(game.GetValidMoves(player2).empty()==true))){ 
 		  const int n1 = game.Count(player1); 
 		  const int n2 = game.Count(player2); 
 		  std::cout << "The game has ended." << std::endl 
 			<< "Player 1 conquered " << n1 << " squares." << std::endl 
-			<< "Player 2 conquered " << n2 << " squares." << std::endl 
-			<< "The winner is player " << (n1 > n2 ? "1" : "2") << std::endl 
-			<< "Congratulations!" << std::endl 
-			<< std::endl;
+			<< "Player 2 conquered " << n2 << " squares." << std::endl;
+			if(n1!=n2){
+			std::cout<< "The winner is player " << (n1 > n2 ? "1" : "2") << std::endl 
+			<< "Congratulations!" << std::endl;
+			}
+			else
+				std::cout<<"It's a draw!\n";
+		    std::cout << "Press ENTER to continue... ";
+			std::cin.ignore( std::numeric_limits <std::streamsize> ::max(), '\n' );
+			test=false;
+			AIlevel="OFF";
+			game.SetBoard(newBoard.GetBoard());
 		  return 1;
 		} 
-
-		//Check if other player can actually do a move 
-		if (game.GetValidMoves(player).empty()==true) 
-		{ //if otherPlayer cannot move,
+		if (game.GetValidMoves(player).empty()==true){ //Check if current player can move 
 		  std::cout << "Too bad! Player " << player << " is unabled to do a valid move!"; 
 		  player = (player == player1 ? player2 : player1); 
 		  std::cout << "\nThe next turn again goes to player " << player << "!" << std::endl; 
 		  continue; 
 		} 
-		if(AIlevel!="OFF"&&player!=AIPlayer) {
+		std::cout<<"Current Player:"<<player<<"\n";//Who's turn is it?
+		if(AIlevel!="OFF"&&player==AIPlayer){//Which AI to use
+			if(AIlevel=="EASY")
+				moveRandomly();
+			if(AIlevel=="MEDIUM"){
+				maxDepth=2;
+				std::pair<int,int> bestMove = findBestMove(player);
+				coordinate.first=bestMove.first;coordinate.second=bestMove.second;
+			}
+			if(AIlevel=="HARD"){
+				maxDepth=3;
+				std::pair<int,int> bestMove = findBestMove(player);
+				coordinate.first=bestMove.first;coordinate.second=bestMove.second;
+				//HardAI hAI(game, player);
+				//coordinate = hAI.findMax();
+				//cout<<"MaxFound:"<<coordinate.first<<","<<coordinate.second<<endl;
+			}
+		}
+		if(test==true&&player==player1){
+			maxDepth=3;
+			std::pair<int,int> bestMove = findBestMove(player);
+			coordinate.first=bestMove.first;coordinate.second=bestMove.second;
+		}
+		else if(AIlevel!="OFF"&&player!=AIPlayer&&test==false) {
 			const std::string input = GetInput();
 			const bool isValidCoordinate = IsCoordinate(input, coordinate); 
-			if (isValidCoordinate == false) 
-			{
+			if (isValidCoordinate == false){
 				if(input=="EXIT")
 					return 0;
 				else if(input=="?"){
@@ -415,55 +474,28 @@ int handleGameInput(){
 				} 
 			}
 		} 
-		if (game.IsValidMove(coordinate.first, coordinate.second, player) == false) 
-		{ 
+		if (game.IsValidMove(coordinate.first, coordinate.second, player) == false){ 
 		  std::cout << "ILLEGAL\n";
 		  continue; 
 		}
-		//Actually do the move 
-		game.DoMove(coordinate.first, coordinate.second, player); 
-
-/*		//Check if the game has ended 
-		if (game.Count(empty) == 0) 
-		{ 
-		  //No empty squares 
-		  const int n1 = game.Count(player1); 
-		  const int n2 = game.Count(player2); 
-		  std::cout << "The game has ended." << std::endl 
-			<< "Player 1 conquered " << n1 << " squares." << std::endl 
-			<< "Player 2 conquered " << n2 << " squares." << std::endl 
-			<< "The winner is player " << (n1 > n2 ? "1" : "2") << std::endl 
-			<< "Congratulations!" << std::endl 
-			<< std::endl;
-		  return 1;
-		} 
-
-		//Check if other player can actually do a move 
-		if (game.GetValidMoves(player).empty()==true) 
-		{ //if otherPlayer cannot move,
-		  std::cout << "Too bad! Player " << player << " is unabled to do a valid move!"; 
-		  //player = (player == player1 ? player2 : player1); 
-		  std::cout << "\nThe next turn again goes to player " << player << "!" << std::endl; 
-		  continue; 
-		} */
-		
-		player = (player == player1 ? player2 : player1); 
-		//foreach pair m in getValidMoves; setSquare(m , validMove);
-		
+		game.DoMove(coordinate.first, coordinate.second, player); //Actually do the move 
+		player = (player == player1 ? player2 : player1);//switch to next player
 	}
-	return 0;
+	return 1;
 }
 //Handles user input and display of data 
 int api(std::string commandLine)
 {
-	for(int i=0;i<100;i++)std::cout<<"\n";
-	std::cout<< "WELCOME\n";
-	if(!handlePregameInput())
-		return 0;
-	std::cout<<"Player1"<<"BLACK"<<player1<<"\n"<<RESET;
-	std::cout<<"Player2"<<"WHITE"<<player2<<"\n"<<RESET;
-	if(!handleGameInput())
-		return 0;
+	while(1){
+		for(int i=0;i<100;i++)std::cout<<"\n";
+		std::cout<< "WELCOME\n";
+		if(!handlePregameInput())
+			return 0;
+		std::cout<<"Player1"<<"BLACK"<<player1<<"\n"<<RESET;
+		std::cout<<"Player2"<<"WHITE"<<player2<<"\n"<<RESET;
+		if(!handleGameInput())
+			return 0;
+	}
 }
 
 int main() 
